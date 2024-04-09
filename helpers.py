b5fab7932b7bec0af2f0d963cc013ee201e9afd1
vicclu <ding.victor@gmail.com>
2024-04-09 15:45:51 +0200

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,1 +1,18 @@
 #indeholder alle de funktioner, som der står vi skal skrive.
+
+#funktionsværdier i `f` i intervallet $[Low, Up]$ og angiver de tilhørende `t`-værdier.
+def interval(f, t, low, up):
+    for i in range(0, len(f)):
+        if f[i] > low and f[i] < up:
+            print("Data(f): ", f[i], "Time(t): ", t[i])
+    return None
+        
+
+# Fortegn skift
+def fortegnskift(f, t):
+    for i in range(0, len(f) - 1):
+        if f[i] * f[i+1] < 0:
+            print("Skift ved: ", t[i])
+    if f[len(f) - 1] * f[len(f)- 2] < 0:
+        print("Skift ved: ", t[i])
+    return None
