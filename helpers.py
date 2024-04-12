2d8a66a01f326a862a0ccc8f8b2e9e00d061abfd
Bastian12321 <155532697+Bastian12321@users.noreply.github.com>
2024-04-12 19:10:40 +0200

    Update helpers.py
    added solar panel projection arrays

--- helpers.py
+++ helpers.py
@@ -21,3 +21,13 @@ def fortegnskift(f, t):
 def solar_elevation_angle(theta):
     return np.pi/2 - theta
 
+# solar panel elevation projection funktioner:
+def solar_panel_projection(theta_s, phi_s, theta_p, phi_p):
+    proj = sin(theta_p) * sin(theta_s) * cos(phi_p - phi_s) + cos(theta_p + theta_s)
+    return max(proj,0)
+
+def solar_panel_projection_arrays(theta_s, phi_s, theta_p, phi_p):
+    if (len(theta_s) == len(phi_s) == len(theta_p) == len(phi_p)):
+        return np.array([solar_panel_projection(theta_s[i], phi_s[i], theta_p[i], phi_p[i]) for i in range(len(theta_s))])
+    else: 
+        raise ValueError("Arrays do not have equal number of entries")
