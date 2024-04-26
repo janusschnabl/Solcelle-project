ec3e7d105e477b80d6594390bdfaffa0e4ddcd04
vicclu <ding.victor@gmail.com>
2024-04-26 11:02:59 +0200

    Update Begge_vinkler_måned.py

--- Begge_vinkler_måned.py
+++ Begge_vinkler_måned.py
@@ -85,3 +85,33 @@ plt.grid(True)
 plt.xticks(months)
 plt.show()
 
+
+#Måned energi graf
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+def get_daily_energy(start_date, end_date, lat, lon, alt, theta, phi):
+    result = []
+    for single_date in daterange(start_date, end_date): 
+        date_string = single_date.strftime("%Y-%m-%d")
+        solpos = get_solar_position(date_string, date_string, lat, lon, alt, "Europe/Copenhagen", dt = "Min")
+        flux = PANEL_COUNT * solar_flux(L,B,S0,A0, PANEL_EFFICIENCY, theta, phi, solpos )
+        joule = integrate.simps(flux, dx=60)
+        result.append(joule)
+    return result
+    
+    
+pdStartDates = pd.date_range(date(2024,1,1), date(2024,12,31), freq='MS')
+pdEndDates = pdStartDates + pd.offsets.MonthEnd(1)
+startDates = np.array([d.strftime('%Y-%m-%d') for d in pdStartDates])
+endDates = np.array([d.strftime('%Y-%m-%d') for d in pdEndDates])
+
+PANEL_COUNT = 38
+energy_optimized = []
+for i in range(12):
+    energy_optimized += get_daily_energy(pdStartDates[i], pdEndDates[i], 55.781, 12.5234, 10, thetas[i], phis[i])
+    
+plt.plot(energy_optimized)
+sum(energy_optimized)
