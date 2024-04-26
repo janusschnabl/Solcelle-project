25d1986545c03989e14454205ce4300c021de45d
Thomas Poulsen <s235218@dtu.dk>
2024-04-26 12:59:40 +0200

    Price variable angle

--- helpers.py
+++ helpers.py
@@ -153,21 +153,38 @@ def get_daily_energy(start_date, end_date, lat, lon, alt):
     return result
 
 # Precompute solar positions
-def monthly_angles(longtitude, altitude, elevation, tz, year):
-    pdStartDates = pd.date_range(start='2024-01-01', end='2024-12-01', freq='MS')
+def get_monthly_prices():
+    prices = pd.read_json("Elspotprices.json")
+    prices = prices[prices.PriceArea == "DK2"]
+    prices["HourDK"] = pd.to_datetime(prices["HourDK"])
+    prices["HourUTC"] = pd.to_datetime(prices["HourUTC"])
+    monthly_prices = []
+    for i in range(1,13):
+        month = prices[prices["HourDK"].dt.month == i]
+        month.sort_values(by=["HourDK","HourUTC"])
+        monthly_prices.append(np.array(month["SpotPriceDKK"] / 3600000000))
+    return monthly_prices
+
+# Precompute solar positions
+def monthly_angles(longtitude, altitude, elevation, tz, optimize_price=True):
+    pdStartDates = pd.date_range(start='2023-01-01', end='2023-12-01', freq='MS')
     pdEndDates = pdStartDates + pd.offsets.MonthEnd(1)
     startDates = np.array([d.strftime('%Y-%m-%d') for d in pdStartDates])
     endDates = np.array([d.strftime('%Y-%m-%d') for d in pdEndDates])
+    monthly_prices = get_monthly_prices()
     result = []
     for i in range(12):
         solpos = get_solar_position(startDates[i], endDates[i], longtitude, altitude, elevation, tz)
         energy = np.zeros((91, 360))
         for theta in range(91):
             for phi in range(360):
-                flux = solar_flux(L, B, 1100, 0.5, PANEL_EFFICIENCY,  theta, phi, solpos)
-                energy[theta][phi] = integrate.simps(flux, dx=3600)
+                flux = solar_flux(L, B, 1100, 0.5,  theta, phi, solpos)
+                price = monthly_prices[i] * flux
+                energy[theta][phi] = integrate.simps(price if optimize_price else flux, dx=3600)
         max_energy_index = np.unravel_index(np.argmax(energy), energy.shape)
         result.append(max_energy_index)
+
+        print(f"Maximum energy produced at Theta = {max_energy_index[0]} degrees and Phi = {max_energy_index[1]} degrees")
     return result
 
 # Datoer
--- total_energiprod.py
+++ total_energiprod.py
@@ -1,20 +1,40 @@
-PANEL_COUNT = 38
+from helpers import *
+#from Begge_vinkler_måned import monthly_angles
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
+def get_daily_prices():
+    prices = pd.read_json("Elspotprices.json")
+    prices = prices[prices.PriceArea == "DK2"]
+    prices["HourDK"] = pd.to_datetime(prices["HourDK"])
+    prices["HourUTC"] = pd.to_datetime(prices["HourUTC"])
+    monthly_prices = []
+    for i in range(1,13):
+        month_prices = []
+        month = prices[prices["HourDK"].dt.month == i]
+        for j in range(1,max(month["HourDK"].dt.day)+1):
+            day = month[month["HourDK"].dt.day == j]
+            day.sort_values(by=["HourDK","HourUTC"])
+            month_prices.append(np.array(day["SpotPriceDKK"] / 3600000000))
+        monthly_prices.append(month_prices)
+    return monthly_prices
+
+
 def get_daily_energy(start_date, end_date, lat, lon, alt, theta, phi):
+    daily_prices = get_daily_prices()
     result = []
-    for single_date in daterange(start_date, end_date): 
+    for single_date in daterange(start_date, end_date):
         date_string = single_date.strftime("%Y-%m-%d")
-        solpos = get_solar_position(date_string, date_string, lat, lon, alt, "Europe/Copenhagen", dt = "Min")
-        flux = PANEL_COUNT * solar_flux(L,B,S0,A0, PANEL_EFFICIENCY, theta, phi, solpos )
-        joule = integrate.simps(flux, dx=60)
+        solpos = get_solar_position(date_string, date_string, lat, lon, alt, "Europe/Copenhagen", dt = "H")
+        flux = PANEL_COUNT * solar_flux(L,B,S0,A0, theta, phi, solpos )
+        price = flux * daily_prices[single_date.month-1][single_date.day-1]
+        joule = integrate.simps(price, dx=3600)
         result.append(joule)
     return result
-    
-    
+
+
 pdStartDates = pd.date_range(date(2023,1,1), date(2023,12,31), freq='MS')
 pdEndDates = pdStartDates + pd.offsets.MonthEnd(1)
 startDates = np.array([d.strftime('%Y-%m-%d') for d in pdStartDates])
@@ -22,9 +42,12 @@ endDates = np.array([d.strftime('%Y-%m-%d') for d in pdEndDates])
 
 print(endDates)
 
+angles = monthly_angles(55.7861, 12.5234, 10, "Europe/Copenhagen",optimize_price=True)
+thetas, phis = zip(*angles)
+
 energy_optimized = []
 for i in range(12):
     energy_optimized += get_daily_energy(pdStartDates[i], pdEndDates[i], 55.781, 12.5234, 10, thetas[i], phis[i])
-    
+
 plt.plot(energy_optimized)
-sum(energy_optimized)
+print(energy_optimized)
